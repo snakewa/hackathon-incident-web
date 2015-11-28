4f617892b5b5d51f9aa6f22b0bf73f91528e2682
Steven Lei <stevenplus@gmail.com>
2015-11-28 16:00:51 +0800

    Implement Layout and Google Map

--- src/scripts/components/welcome/index.js
+++ src/scripts/components/welcome/index.js
@@ -5,15 +5,52 @@ import {
     Link
 } from 'react-router';
 import Elemental from 'elemental';
+import { GoogleMap, Marker, SearchBox } from "react-google-maps";
 
 require('elemental/less/elemental.less');
 
+
 let Welcome = React.createClass({
     render: function () {
+
+    	var cards = [];
+
+    	for( var i = 0; i < 20; i++ ) {
+			cards.push(
+				<Elemental.Card>
+					<a href="#">
+						Hello
+					</a>
+				</Elemental.Card>
+			);
+		}
+
         return (
             <div className="welcome">
-                Welcome componet here
-                <Elemental.Button type="primary">Primary</Elemental.Button>
+
+            	<h1 className="u-text-center">Macau Incident Report</h1>
+
+            	<Elemental.Container>
+	            	<Elemental.Row>
+	            		<Elemental.Col lg="1/4">
+	            			{cards}
+	            		</Elemental.Col>
+
+	            		<Elemental.Col lg="3/4">
+	            			<section style={{height: "100%"}}>
+						      <GoogleMap containerProps={{
+						          style: {
+						            height: "100%",
+						          },
+						        }}
+						        defaultZoom={3}
+						        defaultCenter={{lat: -25.363882, lng: 131.044922}}>
+						      </GoogleMap>
+						    </section>
+	            		</Elemental.Col>
+	            	</Elemental.Row>
+            	</Elemental.Container>
+
             </div>
         )
     }
